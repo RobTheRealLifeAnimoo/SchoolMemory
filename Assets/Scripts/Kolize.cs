c9934c7272c0881de42a2b0e29e1f72662a45d65
Aleeees <strnadales2@gmail.com>
2022-02-08 08:49:32 +0100

    Stejně to nefunguje :/

--- Assets/Scripts/Kolize.cs
+++ Assets/Scripts/Kolize.cs
@@ -11,13 +11,26 @@ public class Kolize : MonoBehaviour
     void Start()
     {
         
+        animator=GetComponent<Animator>();
         otevreno = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (otevreno)
+            {
+                animator.SetBool("stav", true);
+                otevreno = false;
+            }
+            else
+            {
+                animator.SetBool("stav", false);
+                otevreno = true;
+            }
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
