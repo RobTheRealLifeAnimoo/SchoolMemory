14af6a538da1b44084d2693173ac717bd357ee2c
Robin Tetour <Rob.Tetour@outlook.com>
2022-02-08 11:05:12 +0100

    Interakce s dveřmi FIXED

--- Assets/Scripts/Kolize.cs
+++ Assets/Scripts/Kolize.cs
@@ -1,34 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Kolize : MonoBehaviour
 {
-    Animator animator;
+    public Animator animator;
     
-    bool otevreno;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-        animator=GetComponent<Animator>();
-        otevreno = true;
-    }
+    bool inRange = false;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F) && inRange)
         {
-            if (otevreno)
+            if (animator.GetBool("stav") == false)
             {
                 animator.SetBool("stav", true);
-                otevreno = false;
             }
             else
             {
                 animator.SetBool("stav", false);
-                otevreno = true;
             }
         }
     }
@@ -36,21 +25,17 @@ public class Kolize : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            Debug.Log("Kruuuuug");
-            if (Input.GetKeyDown(KeyCode.F))
-            {
-                if (otevreno) 
-                {
-                    animator.SetBool("stav", true);
-                    otevreno = false; 
-                }
-                else 
-                {
-                    animator.SetBool("stav", false);
-                    otevreno = true; 
-                }
-            }
+            inRange = true;
         }
         
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inRange = false;
+        }
+
     }
 }
